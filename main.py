f0b31cef922669253b12210e1aaa91add84aa96d
Karan Bhardwaj <karanbhardwaj1210@gmail.com>
2024-11-28 14:58:34 +0530

    stock prompt enhancing

--- main.py
+++ main.py
@@ -305,7 +305,7 @@ def is_stock_query(user_question):
          "What is Microsoft's current stock price?" → "True MSFT"
          "How much is Tesla trading for?" → "True TSLA"
          "What is the price of google?" → "True GOOGL"
-         "What is price of cxpx" → "True CXPX.L"
+         "What is price of cspx" → "True CSPX.L"
          "csndx price" → "True CSNDX.SW"
 
     2. IF the question is about NEWS/ANALYSIS of STOCKS and COMPANIES, respond: "News [REPHRASED_QUERY]"
