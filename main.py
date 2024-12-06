3773c093df0692295ff836ef604340d82ac78ac2
Karan Bhardwaj <karanbhardwaj1210@gmail.com>
2024-12-07 01:04:40 +0530

    Kran Faq

--- main.py
+++ main.py
@@ -951,7 +951,7 @@ def user_input(user_question):
                     The provided answer is:
                     {answer}
 
-                    Based on this information, let me expand on the response within 100 words but do not add anything by assumption:
+                    Based on this information, let me response within 100 words:
 
                     {context}
 
