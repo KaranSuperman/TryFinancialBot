0425a89845bb8e3b87cab5d514ed18f932f94e53
Karan Bhardwaj <karanbhardwaj1210@gmail.com>
2024-12-16 23:50:43 +0530

    normal changes

--- karan_main.py
+++ karan_main.py
@@ -548,7 +548,7 @@ def create_research_chain(exa_api_key: str, gemini_api_key: str):
 
             Output Format:
             **Financial Market Briefing should ne only in plain text**
-            **Source**
+            **Sources with inbuilt link**
             """)
         ])
 
